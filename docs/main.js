a176c32f2ba8eb4dccc18e98bb03f62b27c14564
tochirinuru <102892587+tochirinuru@users.noreply.github.com>
2023-06-08 23:45:56 +0900

    32

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,6 @@
 // モジュールの読み込み
-import OpacityControl from 'maplibre-gl-opacity';
 import 'maplibre-gl-opacity/dist/maplibre-gl-opacity.css';
+import OpacityControl from 'maplibre-gl-opacity';
 
 // 地図1（地理院タイル 淡色地図）の設定
 const map = new maplibregl.Map({
